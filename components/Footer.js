bc2c7359421ba54fb7b4f6788135c0418f20f699
abddulrahmaanHussein <a.hussein@fis.ps>
2021-10-18 23:21:16 +0300

    Add Header and Footer

---
+++ components/Footer.js
@@ -0,0 +1,15 @@
+import Link from 'next/link';
+import styles from '../styles/Footer.module.css'
+
+export default function Footer() {
+    return (
+        <footer className={styles.footer}>
+            <p>Copyright &copy; DJ Events {new Date().getFullYear()}</p>
+            <p>
+                <Link href='/about'>
+                About This Project
+                </Link>
+            </p>
+        </footer>
+    )
+}
