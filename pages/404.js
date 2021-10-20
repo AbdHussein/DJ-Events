63e602674b2181cbf81cd3efd954e94082db23c2
abddulrahmaanHussein <a.hussein@fis.ps>
2021-10-20 17:04:17 +0300

    Create Event Details Page

---
+++ pages/404.js
@@ -0,0 +1,16 @@
+import Link from 'next/link';
+import {FaExclamationTriangle} from 'react-icons/fa'
+import Layout from "@/components/Layout";
+import styles from '@/styles/404.module.css';
+
+export default function NotFoundPage() {
+    return (
+        <Layout title='Page not found'>
+            <div className={styles.error}>
+                <h1><FaExclamationTriangle /> 404</h1>
+                <h4>Sorry, There is nothing here</h4>
+                <Link href='/'>Go to home page</Link>
+            </div>
+        </Layout>
+    )
+}
--- pages/events/index.js
+++ pages/events/index.js
@@ -1,9 +1,32 @@
-import Layout from "../../components/Layout"
+import Layout from "@/components/Layout"
+import { API_URL } from "@/config/index";
+import EventItem from "@/components/EventItem";
 
-export default function EventsPage() {
-    return (
-        <Layout>
-            <h1>Events</h1>
-        </Layout>
-    )
+export default function EventsPage({events}) {
+  return (
+    <Layout>
+      <h1>Events</h1>
+      {
+        events?.length === 0
+        &&
+        <h3>
+          No events to show
+        </h3>
+      }
+      {
+        events?.map(event => (
+          <EventItem key={event.id} evt={event}/>
+        ))
+      }
+    </Layout>
+  )
+}
+
+export async function getStaticProps(){
+  const res = await fetch(`${API_URL}/api/events`);
+  const events = await res.json();
+  return {
+    props: {events},
+    revalidate: 1
+  }
 }
